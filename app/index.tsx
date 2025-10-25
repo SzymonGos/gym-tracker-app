c4143cc25e93ee8e849f2cc8eeb838fb38cd573e
Szymon <szymonngos@gmail.com>
2025-10-25 10:03:47 +0100

    add gluestack ui, nvm, login, register screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,31 @@
+import { Link } from "expo-router";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={{ marginBottom: 10 }}>Welcome to the Gym Tracker App</Text>
+      <Link href="/login">
+        <Text style={styles.link}>Login</Text>
+      </Link>
+      <Link href="/register">
+        <Text style={styles.link}>Register</Text>
+      </Link>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  link: {
+    marginBottom: 10,
+    textDecorationLine: "none",
+  },
+});
